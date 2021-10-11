54d468fbe5629faf1fe7ea9deb08431126e11301
茶粟粟 <3507493195@qq.com>
2021-10-11 11:10:20 +0800

    第一天提交

---
+++ src/views/home/Home.vue
@@ -0,0 +1,43 @@
+<template>
+<div id='home'>
+  <nav-bar class="home-nav"><div slot="center">购物车</div></nav-bar>
+<home-swiper :banners='banners'></home-swiper>
+<recommend-view :recommends="recommends"></recommend-view>
+</div>
+</template>
+
+<script>
+import NavBar from '@/components/common/navbar/NavBar.vue'
+import HomeSwiper from './childComps/HomeSwiper.vue'
+import RecommendView from './childComps/RecommendView.vue'
+import{getHomeMultidata} from '@/network/home.js'
+export default {
+name:'Home',
+components:{
+NavBar,
+HomeSwiper,
+RecommendView
+},
+data(){
+  return{
+    banners:[],
+    recommends:[]
+  }
+},
+created(){
+  getHomeMultidata().then(res=>{
+    // 拿到服务器所有数据
+    console.log(res);
+    this.banners=res.data.banner.list
+    this.recommends=res.data.recommend.list
+  })
+}
+}
+</script>
+
+<style>
+.home-nav{
+  background-color: var(--color-tint);
+  color: #f6f6ff ;
+}
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class='recommend'>
+    <div v-for='(item,index) in recommends' :key="index" class='recommendItem'>
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name:'RecommendView',
+props:{
+  recommends:{
+    type:Array,
+    default(){
+      return[]
+    }
+  }
+}
+}
+</script>
+
+<style>
+.recommend{
+  display: flex;
+  width: 100%;
+  text-align: center;
+  padding: 10px 0  15px;
+  font-size: 12px;
+  border-bottom: 10px solid #eee;
+}
+.recommendItem{
+  flex: 1;
+}
+.recommendItem img{
+  height: 65px;
+  width: 65px;
+  margin-bottom: 10px;
+}
+</style>
